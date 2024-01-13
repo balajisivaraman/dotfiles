2a381013afe2d667c5e78c03221c84de337a91f4
Balaji Sivaraman <balaji@balajisivaraman.com>
2024-01-13 15:12:57 +0530

    feat: add env.nix

---
+++ env.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> {}; [
+  bat
+  chezmoi
+  delta
+  devbox
+  eza
+  fd
+  fnm
+  fzf
+  git
+  neovim
+  podman
+  python311Full
+  python311Packages.pynvim
+  ripgrep
+  starship
+  zoxide
+]
+
